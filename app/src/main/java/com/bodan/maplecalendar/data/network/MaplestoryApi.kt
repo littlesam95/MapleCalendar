3aaeaed9989429a8c7a0def5db7699732581235f
littlesam95 <littlesam95@naver.com>
2023-12-26 19:45:17 +0900

    [feat] API 통신 로직 작성
    1. GET character ocid : 마지막으로 설정하고 로컬에 저장한 닉네임으로 ocid를 받아옴
    2. GET character basic : ocid로 캐릭터의 정보(월드, 레벨, 길드, 캐릭터 이미지)를 받아옴
    3. GET character power : ocid로 캐릭터의 스탯(전투력)을 받아옴

---
+++ app/src/main/java/com/bodan/maplecalendar/data/network/MaplestoryApi.kt
@@ -0,0 +1,28 @@
+package com.bodan.maplecalendar.data.network
+
+import com.bodan.maplecalendar.data.dto.CharacterBasic
+import com.bodan.maplecalendar.data.dto.CharacterOcid
+import com.bodan.maplecalendar.data.dto.CharacterStat
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface MaplestoryApi {
+
+    @GET("v1/id")
+    suspend fun fetchCharacterOcid(
+        @Query("character_name") characterName: String
+    ): Response<CharacterOcid>
+
+    @GET("v1/character/basic")
+    suspend fun fetchCharacterBasic(
+        @Query("ocid") ocid: String,
+        @Query("date") date: String
+    ): Response<CharacterBasic>
+
+    @GET("v1/character/stat")
+    suspend fun fetchCharacterStat(
+        @Query("ocid") ocid: String,
+        @Query("date") date: String
+    ): Response<CharacterStat>
+}
