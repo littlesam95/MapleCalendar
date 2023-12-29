ccb0f13cfba46048be6c3b061a8cc98875eb0422
littlesam95 <littlesam95@naver.com>
2023-12-30 01:27:17 +0900

    [feat] 달력 UI 추가

---
+++ app/src/main/java/com/bodan/maplecalendar/presentation/calendar/DayEventFragment.kt
@@ -0,0 +1,44 @@
+package com.bodan.maplecalendar.presentation.calendar
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.lifecycleScope
+import com.bodan.maplecalendar.R
+import com.bodan.maplecalendar.databinding.FragmentDayEventBinding
+import com.bodan.maplecalendar.presentation.BaseDialogFragment
+import com.bodan.maplecalendar.presentation.MainViewModel
+import com.bodan.maplecalendar.presentation.lobby.EventListAdapter
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
+
+class DayEventFragment : BaseDialogFragment<FragmentDayEventBinding>(R.layout.fragment_day_event) {
+
+    private val viewModel: MainViewModel by activityViewModels()
+    private lateinit var eventListAdapter: EventListAdapter
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initListAdapter()
+        initRecyclerView()
+
+        binding.vm = viewModel
+        binding.listAdapter = eventListAdapter
+        isCancelable = false
+
+        lifecycleScope.launch {
+            viewModel.calendarUiEvent.collectLatest { uiEvent ->
+                if (uiEvent == CalendarUiEvent.CloseEventsOfDate) dismiss()
+            }
+        }
+    }
+
+    private fun initListAdapter() {
+        eventListAdapter = EventListAdapter()
+    }
+
+    private fun initRecyclerView() {
+        binding.rvDayEvent.setHasFixedSize(false)
+    }
+}
