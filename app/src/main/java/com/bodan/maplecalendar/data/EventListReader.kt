430596fc145344a7bb38d7c5628e7e191c9b1bf0
littlesam95 <littlesam95@naver.com>
2024-01-05 17:57:12 +0900

    [feat] 이벤트 리스트 DB 읽기 로직 변경

--- app/src/main/java/com/bodan/maplecalendar/data/EventListReader.kt
+++ app/src/main/java/com/bodan/maplecalendar/data/EventListReader.kt
@@ -15,69 +15,7 @@ class EventListReader {
     private val db = Firebase.firestore
     private val formatter = SimpleDateFormat("yyyy-MM-dd")
 
-    suspend fun getEventList(): List<EventItem>? {
-        val newEvents = mutableListOf<EventItem>()
-        var flag = true
-
-        db.collection("EventList")
-            .get()
-            .addOnSuccessListener { result ->
-                for (element in result) {
-                    val newEvent = EventItem(
-                        eventName = element.data["eventName"].toString(),
-                        eventIat = element.data["eventIat"].toString(),
-                        eventExp = element.data["eventExp"].toString(),
-                        eventType = when (element.data["eventType"].toString()) {
-                            "BURNING" -> {
-                                EventType.BURNING
-                            }
-
-                            "COORDINATE" -> {
-                                EventType.COORDINATE
-                            }
-
-                            "PCROOM" -> {
-                                EventType.PCROOM
-                            }
-
-                            "COINSHOP" -> {
-                                EventType.COINSHOP
-                            }
-
-                            "CASH" -> {
-                                EventType.CASH
-                            }
-
-                            "WORLDLEAP" -> {
-                                EventType.WORLDLEAP
-                            }
-
-                            "EVENTWORLD" -> {
-                                EventType.EVENTWORLD
-                            }
-
-                            "HUNTING" -> {
-                                EventType.HUNTING
-                            }
-
-                            else -> {
-                                EventType.DEFAULT
-                            }
-                        },
-                        eventUrl = element.data["eventUrl"].toString(),
-                        eventImage = element.data["eventImage"].toString()
-                    )
-                    newEvents.add(newEvent)
-                }
-            }
-            .addOnFailureListener {
-                flag = false
-            }.await()
-
-        return if (flag) newEvents else null
-    }
-
-    suspend fun getEventListOfDate(specificDay: String): List<EventItem>? {
+    suspend fun getEventList(specificDay: String): List<EventItem>? {
         val newEvents = mutableListOf<EventItem>()
         var flag = true
 
@@ -90,7 +28,7 @@ class EventListReader {
                     val compareToIat = iat?.compareTo(formatter.parse(specificDay))
                     val compareToExp = exp?.compareTo(formatter.parse(specificDay))
                     if ((compareToIat != null) && (compareToExp != null)) {
-                        if ((compareToIat < 0) && (compareToExp > 0)) {
+                        if ((compareToIat <= 0) && (compareToExp >= 0)) {
                             val newEvent = EventItem(
                                 eventName = element.data["eventName"].toString(),
                                 eventIat = element.data["eventIat"].toString(),
