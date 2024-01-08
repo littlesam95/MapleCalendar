c9f6633a6f159a312fd1e5697256fa5a46b99f65
littlesam95 <littlesam95@naver.com>
2024-01-08 23:23:03 +0900

    [chore] TedPermission 라이브러리에 대한 의존성 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -100,6 +100,8 @@ dependencies {
 
     implementation("com.jakewharton.timber:timber:5.0.1")
 
+    implementation("io.github.ParkSangGwon:tedpermission-normal:3.3.0")
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
