5aaf1307b4258abc2edb1adf8e9760e26d80176c
littlesam95 <littlesam95@naver.com>
2024-02-20 22:09:10 +0900

    [fix] Keep Annotation 추가

--- app/src/main/java/com/bodan/maplecalendar/data/dto/FinalStat.kt
+++ app/src/main/java/com/bodan/maplecalendar/data/dto/FinalStat.kt
@@ -1,7 +1,9 @@
 package com.bodan.maplecalendar.data.dto
 
+import androidx.annotation.Keep
 import com.squareup.moshi.Json
 
+@Keep
 data class FinalStat(
     @Json(name = "stat_name")
     val statName: String,
