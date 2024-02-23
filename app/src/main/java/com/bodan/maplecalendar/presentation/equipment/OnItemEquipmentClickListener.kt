9d5b88a1c9fc42ac506d973078b2aaf6df520dbc
littlesam95 <littlesam95@naver.com>
2024-02-23 21:41:43 +0900

    [feat] 특정 장비 선택 시 장비의 세부 정보에 대한 DialogFragment를 띄워주도록 Click Listener 함수 재정의

--- app/src/main/java/com/bodan/maplecalendar/presentation/equipment/OnItemEquipmentClickListener.kt
+++ app/src/main/java/com/bodan/maplecalendar/presentation/equipment/OnItemEquipmentClickListener.kt
@@ -2,23 +2,5 @@ package com.bodan.maplecalendar.presentation.equipment
 
 interface OnItemEquipmentClickListener {
 
-    fun onItemEquipmentDefaultClicked(equipmentUiState: EquipmentUiState.EquipmentDefault)
-
-    fun onItemEquipmentAndroidClicked(equipmentUiState: EquipmentUiState.EquipmentAndroid)
-
-    fun onItemEquipmentBadgeClicked(equipmentUiState: EquipmentUiState.EquipmentBadge)
-
-    fun onItemEquipmentEmblemClicked(equipmentUiState: EquipmentUiState.EquipmentEmblem)
-
-    fun onItemEquipmentHeartClicked(equipmentUiState: EquipmentUiState.EquipmentHeart)
-
-    fun onItemEquipmentPocketClicked(equipmentUiState: EquipmentUiState.EquipmentPocket)
-
-    fun onItemEquipmentSeedringClicked(equipmentUiState: EquipmentUiState.EquipmentSeedring)
-
-    fun onItemEquipmentShieldClicked(equipmentUiState: EquipmentUiState.EquipmentShield)
-
-    fun onItemEquipmentSubweaponClicked(equipmentUiState: EquipmentUiState.EquipmentSubweapon)
-
-    fun onItemEquipmentWeaponClicked(equipmentUiState: EquipmentUiState.EquipmentWeapon)
+    fun onItemEquipmentClicked(item: EquipmentUiState.EquipmentOption)
 }
