fb0d2c37dc03b3920e3196df4cdff1360b9c0f6c
littlesam95 <littlesam95@naver.com>
2024-02-23 21:40:12 +0900

    [style] 장비의 세부적인 옵션을 출력하는 화면 UI 구성
    1. 소울 이름(무기인 경우)
    2. 장비 이름(업그레이드 횟수가 1 이상인 경우 옆에 업그레이드 횟수 추가)
    3. 장비 이미지
    4. 장비 분류
    5. 옵션(18가지)
    6. 업그레이드 가능 횟수(복구 가능 횟수0
    7. 황금 망치 사용 유무
    8. 가횟
    9. 잠재능력
    10. 에디셔널 잠재능력
    11. 소울 능력치(무기인 경우)
    12. 아이템 설명
    13. 시드링 레벨(시드링인 경우)

---
+++ app/src/main/java/com/bodan/maplecalendar/presentation/equipment/ItemEquipmentDetailFragment.kt
@@ -0,0 +1,33 @@
+package com.bodan.maplecalendar.presentation.equipment
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.activityViewModels
+import com.bodan.maplecalendar.R
+import com.bodan.maplecalendar.databinding.FragmentItemEquipmentDetailBinding
+import com.bodan.maplecalendar.presentation.BaseDialogFragment
+import com.bodan.maplecalendar.presentation.CharacterViewModel
+
+class ItemEquipmentDetailFragment : BaseDialogFragment<FragmentItemEquipmentDetailBinding>(R.layout.fragment_item_equipment_detail) {
+
+    private val viewModel: CharacterViewModel by activityViewModels()
+    private lateinit var itemEquipmentDetailOptionListAdapter: ItemEquipmentDetailOptionListAdapter
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initListAdapter()
+        initRecyclerView()
+
+        binding.vm = viewModel
+        binding.listAdapter = itemEquipmentDetailOptionListAdapter
+    }
+
+    private fun initListAdapter() {
+        itemEquipmentDetailOptionListAdapter = ItemEquipmentDetailOptionListAdapter()
+    }
+
+    private fun initRecyclerView() {
+        binding.rvOptionItemEquipmentDetail.setHasFixedSize(false)
+    }
+}
