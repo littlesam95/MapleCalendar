a787a27b6db88f4a2d2b59b9cb94b825304e46b3
littlesam95 <littlesam95@naver.com>
2024-02-23 21:51:06 +0900

    [feat] 장비의 총합, 기본, 추가, 기타(주흔 및 주문서), 스타포스, 익셉셔널로 증가하는 18가지의 옵션을 RecyclewView로 나타내도록 하기 위한 데이터 클래스 및 ListAdapter 선언

---
+++ app/src/main/java/com/bodan/maplecalendar/presentation/equipment/ItemEquipmentDetailOptionListAdapter.kt
@@ -0,0 +1,49 @@
+package com.bodan.maplecalendar.presentation.equipment
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.bodan.maplecalendar.databinding.ItemEquipmentDetailOptionBinding
+
+class ItemEquipmentDetailOptionListAdapter :
+    ListAdapter<EquipmentDetailUiState, RecyclerView.ViewHolder>(equipmentDetailUiStateDiffUtil) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        return ItemEquipmentDetailOptionViewHolder(
+            ItemEquipmentDetailOptionBinding.inflate(
+                LayoutInflater.from(parent.context), parent, false
+            )
+        )
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        (holder as ItemEquipmentDetailOptionViewHolder).bind(currentList[position] as EquipmentDetailUiState.EquipmentDetailOption)
+    }
+
+    companion object {
+        val equipmentDetailUiStateDiffUtil =
+            object : DiffUtil.ItemCallback<EquipmentDetailUiState>() {
+                override fun areItemsTheSame(
+                    oldItem: EquipmentDetailUiState,
+                    newItem: EquipmentDetailUiState
+                ) =
+                    (oldItem.id == newItem.id)
+
+                override fun areContentsTheSame(
+                    oldItem: EquipmentDetailUiState,
+                    newItem: EquipmentDetailUiState
+                ) =
+                    (oldItem == newItem)
+            }
+    }
+
+    class ItemEquipmentDetailOptionViewHolder(private val binding: ItemEquipmentDetailOptionBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(detailOption: EquipmentDetailUiState.EquipmentDetailOption) {
+            binding.detailOption = detailOption
+        }
+    }
+}
