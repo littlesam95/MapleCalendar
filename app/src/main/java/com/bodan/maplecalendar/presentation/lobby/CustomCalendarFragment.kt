1e11847978bce430c21290e581cfb1a5cafdfbbe
littlesam95 <littlesam95@naver.com>
2024-04-24 23:09:49 +0900

    [feat] ViewPager2에 쓰일 Fragment

--- app/src/main/java/com/bodan/maplecalendar/presentation/lobby/CustomCalendarFragment.kt
+++ app/src/main/java/com/bodan/maplecalendar/presentation/lobby/CustomCalendarFragment.kt
@@ -5,42 +5,50 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.bodan.maplecalendar.R
+import androidx.fragment.app.activityViewModels
 import com.bodan.maplecalendar.databinding.FragmentCustomCalendarBinding
-import com.bodan.maplecalendar.presentation.utils.CalendarUtils.getMonthList
-import org.joda.time.DateTime
+import com.bodan.maplecalendar.presentation.MainViewModel
+import com.bodan.maplecalendar.presentation.utils.CalendarUtils.getDaysOfMonth
 
 class CustomCalendarFragment : Fragment() {
 
+    private val viewModel: MainViewModel by activityViewModels()
     private lateinit var binding: FragmentCustomCalendarBinding
-    private var millis: Long = 0L
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-        arguments?.let { argument ->
-            millis = argument.getLong("MILLIS")
-        }
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-
         binding = FragmentCustomCalendarBinding.inflate(inflater, container, false)
-
-        binding.millis.text = DateTime(millis).toString("yyyy-MM")
-        binding.calendarview.initCalendarView(DateTime(millis), getMonthList(DateTime(millis)))
 
         return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.vm = viewModel
+
+        arguments?.let { argument ->
+            val year = argument.getLong("year").toInt()
+            val month = argument.getLong("month").toInt()
+            val text = "${year}년 ${month}월"
+            binding.tvDateInfoCustomCalendar.text = text
+            binding.calendarViewCustomCalendar.initCalendarView(
+                year,
+                month,
+                getDaysOfMonth(year, month),
+                viewModel
+            )
+        }
+    }
+
     companion object {
 
-        fun newInstance(millis: Long) = CustomCalendarFragment().apply {
+        fun newInstance(itemId: Long) = CustomCalendarFragment().apply {
             arguments = Bundle().apply {
-                putLong("MILLIS", millis)
+                putLong("year", itemId / 100L)
+                putLong("month", itemId % 100L)
             }
         }
     }
