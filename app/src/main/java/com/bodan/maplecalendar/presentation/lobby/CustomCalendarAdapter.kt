55812204d00211f5c415bad9fb342e7f1896655c
littlesam95 <littlesam95@naver.com>
2024-04-24 23:03:37 +0900

    [feat] ViewPager2에 쓰일 Adapter 수정
    달력의 시작은 마지막으로 로컬에 저장된 캐릭터 정보 조회 날짜를 기준으로 함. 왼쪽으로 스와이프하면 전 월로 이동하고 오른쪽으로 스와이프하면 다음 월로 이동. View의 Id는 년도 * 100 + 월로 함.

--- app/src/main/java/com/bodan/maplecalendar/presentation/lobby/CustomCalendarAdapter.kt
+++ app/src/main/java/com/bodan/maplecalendar/presentation/lobby/CustomCalendarAdapter.kt
@@ -2,27 +2,59 @@ package com.bodan.maplecalendar.presentation.lobby
 
 import androidx.fragment.app.FragmentActivity
 import androidx.viewpager2.adapter.FragmentStateAdapter
-import org.joda.time.DateTime
+import com.bodan.maplecalendar.app.MainApplication
+import com.bodan.maplecalendar.presentation.DateFormatConverter
+import kotlin.math.abs
 
 class CustomCalendarAdapter(fm: FragmentActivity) : FragmentStateAdapter(fm) {
 
-    private var startDay: Long = DateTime().withDayOfMonth(1).withTimeAtStartOfDay().millis
+    private val dateFormatConverter = DateFormatConverter()
 
     override fun getItemCount(): Int = Int.MAX_VALUE
 
     override fun createFragment(position: Int): CustomCalendarFragment {
-        val millis = getItemId(position)
+        val itemId = getItemId(position)
 
-        return CustomCalendarFragment.newInstance(millis)
+        return CustomCalendarFragment.newInstance(itemId)
     }
 
-    override fun getItemId(position: Int): Long =
-        DateTime(startDay).plusMonths(position - START_POSITION).millis
+    override fun getItemId(position: Int): Long {
+        val searchDate =
+            MainApplication.mySharedPreferences.getSearchDate("searchDate", null) ?: return 0
+        var currentYear = searchDate.substring(0, 4).toInt()
+        var currentMonth = searchDate.substring(5, 7).toInt()
+
+        val move = position - START_POSITION
+        val bias = if (move < 0) -1 else 1
+
+        val moveYear = abs(move) / 12 * bias
+        val moveMonth = abs(move) % 12 * bias
+
+        currentYear += moveYear
+        when {
+            (currentMonth + moveMonth) < 1 -> {
+                currentMonth = 12 + (currentMonth + moveMonth)
+                currentYear--
+            }
+
+            (currentMonth + moveMonth) > 12 -> {
+                currentMonth = (currentMonth + moveMonth) - 12
+                currentYear++
+            }
+
+            else -> {
+                currentMonth = (currentMonth + moveMonth)
+            }
+        }
+
+        return (currentYear * 100 + currentMonth).toLong()
+    }
 
     override fun containsItem(itemId: Long): Boolean {
-        val date = DateTime(itemId)
+        val nowMonth =
+            ((dateFormatConverter.todayYear() * 100) + dateFormatConverter.todayMonth() + 1).toLong()
 
-        return ((date.dayOfMonth == 1) && (date.millisOfDay == 0))
+        return ((itemId > 202311L) && (itemId < nowMonth))
     }
 
     companion object {
