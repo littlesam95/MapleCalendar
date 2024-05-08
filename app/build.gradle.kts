609bc24c0beb1e7cf2a4272ea8db3fe41945bed5
littlesam95 <littlesam95@naver.com>
2024-05-08 12:44:47 +0900

    [chore] 색상 및 문자열 추가, 버전 변경

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,8 +24,8 @@ android {
         applicationId = "com.bodan.maplecalendar"
         minSdk = 28
         targetSdk = 34
-        versionCode = 19
-        versionName = "0.4.3"
+        versionCode = 20
+        versionName = "0.4.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
