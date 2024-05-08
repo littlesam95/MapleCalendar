b75cb16d3682aa5a1971941d49ac7cd9ad25760f
littlesam95 <littlesam95@naver.com>
2024-05-08 12:43:35 +0900

    [fix] ViewHolder에서 TextView의 Visibility를 초기화함으로써 장부 세부정보 UI가 덩실거리는 현상 제거

--- app/src/main/java/com/bodan/maplecalendar/presentation/views/equipment/ItemEquipmentDetailOptionListAdapter.kt
+++ app/src/main/java/com/bodan/maplecalendar/presentation/views/equipment/ItemEquipmentDetailOptionListAdapter.kt
@@ -1,6 +1,7 @@
 package com.bodan.maplecalendar.presentation.views.equipment
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
@@ -43,7 +44,18 @@ class ItemEquipmentDetailOptionListAdapter :
         RecyclerView.ViewHolder(binding.root) {
 
         fun bind(detailOption: EquipmentDetailUiState.EquipmentDetailOption) {
-            binding.detailOption = detailOption
+            with(binding) {
+                this.detailOption = detailOption
+                clDetailOption.visibility = if (detailOption.isZero) View.GONE else View.VISIBLE
+                tvOptionTypeDetailOption.visibility = if (detailOption.isZero) View.GONE else View.VISIBLE
+                tvTotalOptionDetailOption.visibility = if (detailOption.isZero) View.GONE else View.VISIBLE
+                tvLeftBracketDetailOption.visibility = if (detailOption.isForced) View.VISIBLE else View.GONE
+                tvBaseOptionDetailOption.visibility = if (detailOption.isForced) View.VISIBLE else View.GONE
+                tvAddOptionDetailOption.visibility = if (detailOption.addOption.isBlank()) View.GONE else View.VISIBLE
+                tvEtcOptionDetailOption.visibility = if (detailOption.etcOption.isBlank()) View.GONE else View.VISIBLE
+                tvStarforceOptionDetailOption.visibility = if (detailOption.starforceOption.isBlank()) View.GONE else View.VISIBLE
+                tvRightBracketDetailOption.visibility = if (detailOption.isForced) View.VISIBLE else View.GONE
+            }
         }
     }
 }
