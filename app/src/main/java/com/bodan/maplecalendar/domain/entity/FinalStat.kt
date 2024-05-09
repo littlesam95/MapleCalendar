3add38b7d6c5dab03af94d21d084cb3e673b8e7e
littlesam95 <littlesam95@naver.com>
2024-05-09 17:16:35 +0900

    [refactor] Data Layer의 Entity는 네트워크 데이터를 받아오는 데 사용하고, Domain Layer의 데이터 클래스는 UI Layer에서 데이터를 렌더링하는 데 사용하도록 분리

--- app/src/main/java/com/bodan/maplecalendar/domain/entity/FinalStat.kt
+++ app/src/main/java/com/bodan/maplecalendar/domain/entity/FinalStat.kt
@@ -1,15 +1,9 @@
 package com.bodan.maplecalendar.domain.entity
 
 import androidx.annotation.Keep
-import com.squareup.moshi.Json
-import com.squareup.moshi.JsonClass
 
 @Keep
-@JsonClass(generateAdapter = true)
 data class FinalStat(
-    @Json(name = "stat_name")
     val statName: String,
-
-    @Json(name = "stat_value")
     val statValue: String?
 )
--- app/src/main/java/com/bodan/maplecalendar/domain/entity/ItemTitle.kt
+++ app/src/main/java/com/bodan/maplecalendar/domain/entity/ItemTitle.kt
@@ -1,24 +1,12 @@
 package com.bodan.maplecalendar.domain.entity
 
 import androidx.annotation.Keep
-import com.squareup.moshi.Json
-import com.squareup.moshi.JsonClass
 
 @Keep
-@JsonClass(generateAdapter = true)
 data class ItemTitle(
-    @Json(name = "title_name")
     val itemTitleName: String,
-
-    @Json(name = "title_icon")
     val itemTitleIcon: String,
-
-    @Json(name = "title_description")
     val itemTitleDescription: String,
-
-    @Json(name = "date_expire")
     val itemTitleDateExpire: String?,
-
-    @Json(name = "date_option_expire")
     val itemTitleDateOptionExpire: String?
 )
