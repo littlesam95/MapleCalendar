8c40cc6499a2dee23dd74264297ce98afdf04269
littlesam95 <littlesam95@naver.com>
2024-05-24 16:30:06 +0900

    [chore] 문자열 추가, 버전 변경

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,8 +24,8 @@ android {
         applicationId = "com.bodan.maplecalendar"
         minSdk = 28
         targetSdk = 34
-        versionCode = 26
-        versionName = "0.6.0"
+        versionCode = 28
+        versionName = "0.6.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
